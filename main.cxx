d1a3a3932acfd1d3806a397bacc6af4fdeaf288c
Sherlock Wisdom <wisdomnji@gmail.com>
2020-12-12 21:32:18 +0100

    compiles now reads file

--- main.cxx
+++ main.cxx
@@ -2,6 +2,7 @@
 #include "PDFNetC64/Headers/PDF/PDFNet.h"
 #include "PDFNetC64/Headers/PDF/PDFDoc.h"
 #include "PDFNetC64/Headers/PDF/PDFDraw.h"
+#include "PDFNetC64/Headers/PDF/TextExtractor.h"
 
 using namespace std;
 using namespace pdftron;
@@ -10,4 +11,26 @@ using namespace PDF;
 
 int main() {
 	PDFNet::Initialize();
+	try {
+		PDFDoc doc("text_classification_algorithms.pdf");
+		doc.InitSecurityHandler();
+
+		Page page = doc.GetPage(1);
+
+		TextExtractor txt;
+		txt.Begin( page );
+
+		// Extract words one by one
+		TextExtractor::Line line = txt.GetFirstLine();
+		TextExtractor::Word word;
+
+		for(; line.IsValid(); line=line.GetNextLine()) {
+			for(word=line.GetFirstWord(); word.IsValid(); word=word.GetNextWord()) {
+				cout << word.GetString() << endl;
+			}
+		}
+	}
+	catch( pdftron::Common::Exception& ex) {
+		cout << ex.GetMessage() << endl;
+	}
 }
